ee6b85a72e8f8eff4b31d2720248589f1e756e15
User <ann2001901225@gmail.com>
2022-03-18 16:20:45 +0800

    SilverCustomer

--- src/main/java/com/Ann/SalesTester.java
+++ src/main/java/com/Ann/SalesTester.java
@@ -4,9 +4,11 @@ public class SalesTester {
     public static void main(String[] args){
         Customer c1 = new Customer("0001",1200);
         Customer c2 = new Customer("002",500);
-        SilverCustomer c3 = new SilverCustomer("003",5000);
+        SilverCustomer c3 = new SilverCustomer("003",3000);
+        GoldenCustomer c4 = new GoldenCustomer("004",5000);
         c1.print();
         c2.print();
         c3.print();
+        c4.print();
     }
 }
